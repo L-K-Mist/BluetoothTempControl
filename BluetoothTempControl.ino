fd0271470032aa5ac7558b52cefc6592731dea60
l-k-mist <dylan@vandenbosch.co.za>
2021-01-17 21:47:31 +0200

    before pivot to better incoming string handling

--- BluetoothTempControl.ino
+++ BluetoothTempControl.ino
@@ -1,28 +1,17 @@
-/*
-  Serial Call and Response in ASCII
-  Language: Wiring/Arduino
-
-  This program sends an ASCII A (byte of value 65) on startup and repeats that
-  until it gets some data in. Then it waits for a byte in the serial port, and
-  sends three ASCII-encoded, comma-separated sensor values, truncated by a
-  linefeed and carriage return, whenever it gets a byte in.
-
-  The circuit:
-  - potentiometers attached to analog inputs 0 and 1
-  - pushbutton attached to digital I/O 2
-
-  created 26 Sep 2005
-  by Tom Igoe
-  modified 24 Apr 2012
-  by Tom Igoe and Scott Fitzgerald
-  Thanks to Greg Shakar and Scott Fitzgerald for the improvements
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/SerialCallResponseASCII
-*/
-
 #include <SoftwareSerial.h>
+
+#include "thermistor.h"
+// Analog pin used to read the NTC
+#define NTC_PIN A0
+
+#include "TempSensor.h"
+#include "Heater.h"
+
+TempSensor tempSensor(NTC_PIN);
+Heater heater(&tempSensor);
+
+// Global temperature reading
+float temp;
 
 SoftwareSerial BluetoothDevice(3,4);
 
@@ -30,32 +19,35 @@ String inString = "";         // incoming serial byte
 String str_ii = "";
 
 void setup() {
+  heater.setup();
+
   Serial.begin(115200);
   BluetoothDevice.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   delay(100);
-  // while (!BluetoothDevice) {
-  //   ; // wait for serial port to connect. Needed for native USB port only
-  // }
-
   establishContact();  // send a byte to establish contact until receiver responds
 }
 
 void loop() {
+  // temp = heater.getTempC();
+
   // if we get a valid byte, read analog ins:
-  if(BluetoothDevice.available() > 0){
+  // if(BluetoothDevice.available() > 0){
     while (BluetoothDevice.available() > 0) {
+        char endMarker = '\n';
         char s = BluetoothDevice.read();
         inString += s;
-        Serial.print(s);
+        // Serial.print(s);
         delay(100);
       // getString();
     }
      Serial.print("We got a string: ");
      Serial.println(inString);
-  }
+     executeIncomingCommand(inString);
+
+  // }
   delay(100);
 }
 // TODO Research async-await like functionality for c++ arduino.
@@ -64,6 +56,15 @@ void establishContact() {
     BluetoothDevice.println("A");   // send an initial string
     delay(600);
   }
+}
+
+void executeIncomingCommand(String command) {
+  Serial.print("Got command: ");
+  Serial.println(command);
+  if(command == "Beautiful\n"){
+    Serial.println("Yip I know it's Beautiful");
+  }
+inString = "";
 }
 
 
